2be17364bf008a16d74b906a7812563da7ee9fca
diegoih98 <53347276+diegoih98@users.noreply.github.com>
2020-03-06 16:29:59 -0600

    Add accurate Optical Properties to the lead-glass

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -33,6 +33,8 @@
 
 #include "DetectorConstruction.hh"
 #include "DetectorMessenger.hh"
+#include "SensitiveDetector.hh"
+#include "EventAction.hh"
 
 #include "G4NistManager.hh"
 #include "G4Material.hh"
@@ -68,10 +70,14 @@
 #include "G4MaterialPropertiesTable.hh"
 
 #include "G4LogicalSkinSurface.hh"
+#include "G4SubtractionSolid.hh"
+#include "G4LogicalBorderSurface.hh"
+
+#include "G4SDManager.hh"
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 DetectorConstruction::DetectorConstruction()
-:G4VUserDetectorConstruction(),fDefaultMaterial(0),fPhysiWorld(0),
+:G4VUserDetectorConstruction()/*, fEventAction(0)*/,fDefaultMaterial(0),fPhysiWorld(0),
  fDetectorMessenger(0)
 {
   // default parameter values of the absorbers
@@ -97,6 +103,7 @@ DetectorConstruction::DetectorConstruction()
 
   // create commands for interactive definition of the calorimeter
   fDetectorMessenger = new DetectorMessenger(this);
+ // fEventAction = new EventAction(this);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -169,24 +176,45 @@ void DetectorConstruction::DefineMaterials()
 
   fDefaultMaterial = fWorldMaterial;
 
-  G4double cal_Energy[]    = { 7.0*eV , 7.07*eV, 7.14*eV };
-  const G4int calnum = sizeof(cal_Energy)/sizeof(G4double);
 
-  G4double cal_RIND[]  = { 1.8 , 1.8, 1.8 };
-  assert(sizeof(cal_RIND) == sizeof(cal_Energy));
-  G4MaterialPropertiesTable* fCalMaterial_mt = new G4MaterialPropertiesTable();
-  fCalMaterial_mt->AddProperty("RINDEX", cal_Energy, cal_RIND,  calnum);
-  fCalMaterial->SetMaterialPropertiesTable(fCalMaterial_mt);
 
- 
-  G4double glass_RIND[]={1.49,1.49,1.49};
-  assert(sizeof(glass_RIND) == sizeof(cal_Energy));
-  G4double glass_AbsLength[]={420.*cm,420.*cm,420.*cm};
-  assert(sizeof(glass_AbsLength) == sizeof(cal_Energy));
-  G4MaterialPropertiesTable *glass_mt = new G4MaterialPropertiesTable();
-  glass_mt->AddProperty("ABSLENGTH",cal_Energy,glass_AbsLength,calnum);
-  glass_mt->AddProperty("RINDEX",cal_Energy,glass_RIND,calnum);
-  fGlass->SetMaterialPropertiesTable(glass_mt);
+  //Lead Glass
+
+    G4double refEnergy[] = {1.24*eV, 1.27*eV, 1.29*eV, 1.32*eV, 1.35*eV, 
+                            1.38*eV, 1.41*eV, 1.44*eV, 1.48*eV, 1.51*eV, 
+                            1.55*eV, 1.59*eV, 1.63*eV, 1.68*eV, 1.72*eV, 
+                            1.77*eV, 1.82*eV, 1.88*eV, 1.94*eV, 2.0*eV, 
+                            2.07*eV, 2.14*eV, 2.21*eV, 2.3*eV, 2.38*eV, 
+                            2.48*eV, 2.58*eV, 2.7*eV, 2.82*eV, 2.95*eV, 
+                            3.1*eV, 3.26*eV, 3.44*eV, 3.65*eV, 3.87*eV,
+                            4.13*eV, 4.43*eV, 4.77*eV, 5.17*eV, 5.64*eV, 6.2*eV};
+    G4double refIndex[] = {1.77568, 1.77635, 1.77705, 1.77779, 1.77858, 
+                            1.77941, 1.78029, 1.78123, 1.78223, 1.7833, 
+                            1.78445, 1.78569, 1.78703, 1.78847, 1.79004, 
+                            1.79176, 1.79363, 1.79569, 1.79796, 1.80048, 
+                            1.80328, 1.80641, 1.80993, 1.81391, 1.81844, 
+                            1.82365, 1.82968, 1.83673, 1.84508, 1.85511, 
+                            1.86739, 1.88274, 1.9025, 1.92879, 1.96527,
+                            2.01844, 2.10029, 2.23367, 2.4627, 2.87238, 3.62362};           
+    const G4int refNbins = sizeof(refEnergy) / sizeof(G4double);
+    assert(sizeof(refEnergy) == sizeof(refIndex));
+    
+
+    G4double absEnergy[] = {0.62*eV, 0.83*eV, 1.17*eV, 1.55*eV, 1.77*eV, 
+                            1.91*eV, 2.07*eV, 2.25*eV, 2.48*eV, 2.58*eV, 
+                            2.7*eV, 2.82*eV, 2.95*eV, 3.1*eV, 3.18*eV, 
+                            3.26*eV, 3.35*eV, 3.44*eV};
+    G4double absLength[] = {79.32*cm, 141.01*cm, 188.99*cm, 572.76*cm, 572.76*cm, 
+                            284.94*cm, 284.94*cm, 284.94*cm, 188.99*cm, 188.99*cm, 
+                            112.23*cm, 93.03*cm, 54.64*cm, 25.80*cm, 14.93*cm, 
+                            7.41*cm, 3.14*cm, 1.04*cm};
+    const G4int absNbins = sizeof(absEnergy) / sizeof(G4double);
+    assert(sizeof(absEnergy) == sizeof(absLength));
+
+    G4MaterialPropertiesTable* lgMPT = new G4MaterialPropertiesTable();
+    lgMPT->AddProperty("RINDEX", refEnergy, refIndex, refNbins)->SetSpline(true);
+    lgMPT->AddProperty("ABSLENGTH", absEnergy, absLength, absNbins)->SetSpline(true);
+    fCalMaterial->SetMaterialPropertiesTable(lgMPT);
 
 }
 
@@ -216,8 +244,9 @@ G4Material* DetectorConstruction::MaterialWithSingleIsotope( G4String name,
 void DetectorConstruction::ComputeParameters()
 {
   fAbsorSizeZ = 360*mm;
-  fWorldSizeZ  = 100.*cm;
-  fWorldSizeXY = 50.*cm;
+  fWorldSizeZ  = (3*100.)*cm;
+  fWorldSizeXY = (5*50.)*cm;
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -266,40 +295,7 @@ G4VPhysicalVolume* DetectorConstruction::ConstructVolumes()
 
   fEcalLength   = 40.*cm;
 
-  G4Box* solidE = new G4Box("VolE",fWorldSizeXY*.4,fWorldSizeXY*.4,fEcalLength/2);
-  G4LogicalVolume* logicECal = 
-    new G4LogicalVolume( solidE,fDefaultMaterial,"VolE");
-  G4VPhysicalVolume* physE = new G4PVPlacement(0,G4ThreeVector(0.,0.,0.),
-                                               "VolE",logicECal,fPhysiWorld,false,0);
-
-
-
- //PMTs
-  G4double innerRadius_pmt = 0*mm;
-  G4double fOuterRadius_pmt = 34*mm;
-  G4double height_pmt = 70*mm;
-  G4double startAngle_pmt = 0*deg;
-  G4double spanningAngle_pmt = 360*deg;
- 
-  G4Tubs* fPmt = new G4Tubs("pmt_tube", innerRadius_pmt, fOuterRadius_pmt, height_pmt, startAngle_pmt, spanningAngle_pmt);
- 
-  G4Tubs* fPhotocath = new G4Tubs("photocath_tube",innerRadius_pmt,fOuterRadius_pmt,height_pmt/2,startAngle_pmt,spanningAngle_pmt);
- 
- fPmt_log = new G4LogicalVolume(fPmt,G4Material::GetMaterial("Glass"),
-                                 "pmt_log");
-  fPhotocath_log = new G4LogicalVolume(fPhotocath,
-                                       G4Material::GetMaterial("Al"),
-                                       "photocath_log");
-
-fPmt_log = new G4LogicalVolume(fPmt,fGlass,
-                                 "pmt_log");
-  fPhotocath_log = new G4LogicalVolume(fPhotocath,
-                                       fAbsMaterial,
-                                       "photocath_log");
- 
-  new G4PVPlacement(0,G4ThreeVector(0,0,-height_pmt/2),fPhotocath_log,"photocath",fPmt_log,false,0);
-
-
+  
 //Aluminium covered
 
 G4double pDz = 170*mm;
@@ -315,22 +311,58 @@ G4double pDy2 = 61*mm;
 G4double pDx4 = 67.5*mm;
 G4double width = 113*mm;
 G4double height = 122*mm;
-G4double gap = 0.2*mm;
+//G4double gap = 0.2*mm;
+//G4double gap = 3.0*mm;
+G4double gap = 2.0*mm;
+G4double gap2 = 3.0*mm;
 
-
-
-G4Trap* solidAl = new G4Trap("ShapeAl",pDz + gap,pTheta,pPhi,pDy1 + gap,pDx1 + gap,pDx2 + gap,pAlp1,pDy2 + gap,pDx3 + gap,pDx4 + gap,pAlp2);
-
-
-  fLogicAl = new G4LogicalVolume(solidAl,fAbsMaterial,"EcalAl");
+G4double TubeG =18.85*cm;
+G4double CathG =20.8*cm;
+G4double Lateral = 135*mm;
+G4double PlateG = 17.1*cm;
 
 
 // Crystals
 
-G4Trap* solidC = new G4Trap("Shape2",pDz,pTheta,pPhi,pDy1,pDx1,pDx2,pAlp1,pDy2,pDx3,pDx4,pAlp2);
+    G4Trap* solidC = new G4Trap("Shape2",pDz,pTheta,pPhi,pDy1,pDx1,pDx2,pAlp1,pDy2,pDx3,pDx4,pAlp2);
+    fLogicCal = new G4LogicalVolume( solidC,fCalMaterial,"Ecal");
 
 
-  fLogicCal = new G4LogicalVolume( solidC,fCalMaterial,"Ecal");
+    G4Tubs *lgTubeSolid = new G4Tubs("LGTubeSolid", 0, 4.3*cm, 1.85*cm, 0, 360.*deg);
+   // G4LogicalVolume* lgTubeLV = new G4LogicalVolume(lgTubeSolid, fCalMaterial, "LGTubeLV");
+   lgTubeLV = new G4LogicalVolume(lgTubeSolid, fCalMaterial, "LGTubeLV");
+
+
+    // Al plate
+    G4Box* alBoxSolid = new G4Box("AlBoxSolid", 6.75*cm, 6.1*cm, 0.1*cm);
+    G4Tubs* alTubeSolid = new G4Tubs("AlTubeSolid", 0, 4.3*cm, 0.1*cm, 0, 360.*deg);
+    G4VSolid* alPlateSolid = new G4SubtractionSolid("AlPlateSolid", alBoxSolid, alTubeSolid, G4Transform3D());
+    alPlateLV = new G4LogicalVolume(alPlateSolid, fAbsMaterial, "AlPlateLV");  
+
+
+
+  /*  // Lead-glass mirror surface
+    G4OpticalSurface* lgMirrorSurface = new G4OpticalSurface("LGMirrorSurface");
+    lgMirrorSurface->SetType(dielectric_LUT);
+    lgMirrorSurface->SetModel(LUT);
+    lgMirrorSurface->SetFinish(polishedlumirrorair);*/
+
+
+    // Lead-glass taped surface
+    G4OpticalSurface* lgTapedSurface = new G4OpticalSurface("LGTapedSurface");
+    lgTapedSurface->SetType(dielectric_LUT);
+    lgTapedSurface->SetModel(LUT);
+    lgTapedSurface->SetFinish(groundfrontpainted);
+
+
+
+    // PMT Photocathode as SD
+    G4Tubs *cathodeSolid= new G4Tubs("CathodeSolid", 0, 4.3*cm, 0.1*cm, 0, 360.*deg);
+    cathodeLV = new G4LogicalVolume(cathodeSolid, fCalMaterial, "CathodeLV");
+   // cathodeLV = new G4LogicalVolume(cathodeSolid, fAbsMaterial, "CathodeLV");
+
+
+
 
 
 // Pb Counters (Crystal + Al cover + PMTs)
@@ -338,13 +370,22 @@ G4Trap* solidC = new G4Trap("Shape2",pDz,pTheta,pPhi,pDy1,pDx1,pDx2,pAlp1,pDy2,p
   G4double T = 3.6;
   G4double angle = std::sin(T*deg);
   G4double angle2 = std::cos(T*deg);
-
+  G4double dis = std::tan(T*deg)*pDz;
 
   G4double x0 = (-(width + gap)*1.0) + (-(pDz+gap)*angle);
+  G4double x01 = (-(Lateral)*1.0 + gap2/2 +4*gap2) + (-(pDz+gap2)*angle);
+
   G4double y  = -(height + gap);
+
+
   G4double zheta0 = -(pDz+gap)*(1-angle2);
+  G4double zheta01 = -(pDz+gap+PlateG)*(1-angle2);
+
   G4double zheta;
+  G4double zheta1;
+
   G4double x;
+  G4double x1;
   G4int k = 1;
   G4int i,j;
 
@@ -353,24 +394,41 @@ G4Trap* solidC = new G4Trap("Shape2",pDz,pTheta,pPhi,pDy1,pDx1,pDx2,pAlp1,pDy2,p
 
  for (i=0; i<3; i++) {
     x  = x0;
+    x1  = x01;
     zheta = zheta0;  
+    zheta1 = zheta01;  
     for (j=0; j<3; j++) {
+
     G4ThreeVector pos2 = G4ThreeVector(x,y,zheta);
     G4Transform3D transform = G4Transform3D(rotm,pos2);
-    G4ThreeVector pos3 = G4ThreeVector(x,y,zheta+pDz+height_pmt-2*gap);
+
+   // G4ThreeVector pos3 = G4ThreeVector(x1,y,zheta+TubeG);
+    G4ThreeVector pos3 = G4ThreeVector(x1,y,zheta+TubeG-3.0*mm);
     G4Transform3D transform3 = G4Transform3D(rotm,pos3);
-    
-      physAl = new G4PVPlacement(transform, "EcalAl", fLogicAl, physE, false, k);
 
-      physPTM = new G4PVPlacement(transform3, "PTM", fPmt_log, physE, false, k);
+    G4ThreeVector pos4 = G4ThreeVector(x,y,zheta-(gap2/2));
+    G4Transform3D transform4 = G4Transform3D(rotm,pos4);
 
-    new G4PVPlacement(0, G4ThreeVector(0.,0.,0.),"Ecal",fLogicCal,physAl,false,k);
+    G4ThreeVector pos6 = G4ThreeVector(x1,y,zheta+17.1*cm -2.0*mm);
+    G4Transform3D transform6 = G4Transform3D(rotm,pos6);
+
+    //G4ThreeVector pos5 = G4ThreeVector(x1,y,zheta+CathG);
+    G4ThreeVector pos5 = G4ThreeVector(x1,y,zheta+CathG-3.0*mm);
+    G4Transform3D transform5 = G4Transform3D(rotm,pos5);
+
+
+    new G4PVPlacement(transform3, "LGTubePV", lgTubeLV, fPhysiWorld, false, k);
+    alPlatePV[k] = new G4PVPlacement(transform6, "AlPlatePV", alPlateLV, fPhysiWorld, false, k);
+    new G4PVPlacement(transform5, "CathodePV", cathodeLV, fPhysiWorld, false, k);
+    physCal[k] = new G4PVPlacement(transform,"Ecal",fLogicCal,fPhysiWorld,false,k);
+
       k++;
-     // T += 3.6;
       angle = std::sin(T*deg);
       angle2 = std::cos(T*deg);
       x += (width + 2*gap) +((pDz+gap)*angle);
+      x1 += (Lateral + -4*gap2 + 2*gap2) +((pDz+gap2)*angle);
       zheta += (pDz+gap)*(1-angle2);
+      zheta1 += (pDz+gap+PlateG)*(1-angle2);
 
        rotm.rotateY(3.6*deg);
     }
@@ -382,20 +440,39 @@ G4Trap* solidC = new G4Trap("Shape2",pDz,pTheta,pPhi,pDy1,pDx1,pDx2,pAlp1,pDy2,p
 
   // surface propoerties
 
-   SurfaceProperties();
+ for (G4int l=1; l<=9; l++) {
+   // new G4LogicalBorderSurface("LGMirrorBorderSurface", physCal[k], fPhysiWorld, lgMirrorSurface);
+    new G4LogicalBorderSurface("LGTapedBorderSurface", physCal[k], alPlatePV[k], lgTapedSurface);
+}
+  SurfaceProperties();
 
   // color regions
 
- logicECal-> SetVisAttributes(G4VisAttributes::GetInvisible());
+   G4Colour myColour(G4Colour::White());
+   G4VisAttributes* magnetVisAtt= new G4VisAttributes(myColour);
+   magnetVisAtt ->SetForceSolid(true);
+    //magnetVisAtt ->SetForceAuxEdgeVisible(true);
+    fLogicCal ->SetVisAttributes(magnetVisAtt);
 
-  G4VisAttributes* regCcolor = new G4VisAttributes(G4Colour(0., 0.7, 0.3));
-  fLogicCal->SetVisAttributes(regCcolor);
- // logicECal->SetVisAttributes(regCcolor);
+ /* G4VisAttributes* regCcolor = new G4VisAttributes(G4Colour(0., 0.7, 0.3));
+  alShieldingLV->SetVisAttributes(regCcolor);*/
 
-  G4VisAttributes* Ccolor = new G4VisAttributes(G4Colour(1.0, 0.0, 0.0));
-  Ccolor -> SetForceAuxEdgeVisible(true);
-  fPmt_log->SetVisAttributes(Ccolor);
-  fLogicAl->SetVisAttributes(Ccolor);
+   G4Colour myColourB(G4Colour::Green());
+  G4VisAttributes* Ccolor = new G4VisAttributes(myColourB);
+ // Ccolor -> SetForceAuxEdgeVisible(true);
+  Ccolor -> SetForceSolid(true);
+  alPlateLV->SetVisAttributes(Ccolor);
+
+  G4VisAttributes* color = new G4VisAttributes(G4Colour(1.0, 0.0, 0.0));
+ // color -> SetForceAuxEdgeVisible(true);
+  color -> SetForceSolid(true);
+  lgTubeLV->SetVisAttributes(color);
+
+  G4VisAttributes* colorr = new G4VisAttributes(G4Colour(0.0, 0.0, 1.0));
+  //colorr -> SetForceAuxEdgeVisible(true);
+  colorr -> SetForceSolid(true);
+   cathodeLV->SetVisAttributes(colorr);
+
   PrintParameters();
 
   //always return the physical World
@@ -422,11 +499,76 @@ void DetectorConstruction::PrintParameters()
 
 
 void DetectorConstruction::SurfaceProperties(){
-  G4double ephoton[] = {7.0*eV, 7.14*eV};
-  const G4int num = sizeof(ephoton)/sizeof(G4double);
 
-  //**Photocathode surface properties
-  G4double photocath_EFF[]={1.,1.}; //Enables 'detection' of photons
+    G4double ephoton[] = {  0.62*eV, 0.83*eV, 1.17*eV, 1.24*eV, 1.27*eV, 
+                            1.29*eV, 1.32*eV, 1.35*eV, 1.38*eV, 1.41*eV,
+                            1.44*eV, 1.48*eV, 1.51*eV, 1.55*eV, 1.59*eV, 
+                            1.63*eV, 1.68*eV, 1.72*eV, 1.77*eV, 1.82*eV, 
+                            1.88*eV, 1.91*eV, 1.94*eV, 2.0*eV,  2.07*eV, 
+                            2.14*eV, 2.21*eV, 2.25*eV, 2.3*eV,  2.38*eV, 
+                            2.48*eV, 2.58*eV, 2.7*eV,  2.82*eV, 2.95*eV, 
+                            3.1*eV,  3.18*eV, 3.26*eV, 3.35*eV, 3.44*eV,
+                            3.65*eV, 3.87*eV, 4.13*eV, 4.43*eV, 4.77*eV, 
+                            5.17*eV, 5.64*eV, 6.2*eV};
+  const G4int num = sizeof(ephoton)/sizeof(G4double);
+    
+   //Al surface properties 
+   G4double refl_Al[] = {1.,1.,1.,1.,1.,
+                         1.,1.,1.,1.,1.,
+                         1.,1.,1.,1.,1.,
+                         1.,1.,1.,1.,1.,
+                         1.,1.,1.,1.,1.,
+                         1.,1.,1.,1.,1.,
+                         1.,1.,1.,1.,1.,
+                         1.,1.,1.,1.,1.,
+                         1.,1.,1.,1.,1.,
+                         1.,1.,1.};
+   assert(sizeof(refl_Al) == sizeof(ephoton));
+   G4double effi_Al[] = {0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0};
+   assert(sizeof(effi_Al) == sizeof(ephoton));
+   G4MaterialPropertiesTable* AlSurfaceProperty = new G4MaterialPropertiesTable();
+   AlSurfaceProperty->AddProperty("REFLECTIVITY",ephoton,refl_Al,num);
+   AlSurfaceProperty->AddProperty("EFFICIENCY",ephoton,effi_Al,num);
+   G4OpticalSurface* AlSurface = 
+    new G4OpticalSurface("AlSurface",glisur,polished,dielectric_metal);
+   AlSurface -> SetMaterialPropertiesTable(AlSurfaceProperty);
+
+
+
+   //Taped Surface
+   G4double refl_Taped[] = {0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0,
+                            0, 0, 0};
+   assert(sizeof(refl_Taped) == sizeof(ephoton));
+   //G4double rindex[] = {0.,0.};
+   //assert(sizeof(effi_Al) == sizeof(ephoton));
+   G4MaterialPropertiesTable* TapedSurfaceProperty = new G4MaterialPropertiesTable();
+   TapedSurfaceProperty->AddProperty("REFLECTIVITY",ephoton,refl_Taped,num);
+   //TapedSurfaceProperty->AddProperty("EFFICIENCY",ephoton,effi_Al,num);
+   G4OpticalSurface* TapedSurface = 
+    new G4OpticalSurface("TapedSurface",unified,polished,dielectric_dielectric);
+   TapedSurface -> SetMaterialPropertiesTable(TapedSurfaceProperty);
+
+  //Photocathode surface properties
+ /* G4double photocath_EFF[]={1.,1.}; //Enables 'detection' of photons
   assert(sizeof(photocath_EFF) == sizeof(ephoton));
   G4double photocath_ReR[]={1.92,1.92};
   assert(sizeof(photocath_ReR) == sizeof(ephoton));
@@ -439,25 +581,15 @@ void DetectorConstruction::SurfaceProperties(){
   G4OpticalSurface* photocath_opsurf=
     new G4OpticalSurface("photocath_opsurf",glisur,polished,
                          dielectric_metal);
-  photocath_opsurf->SetMaterialPropertiesTable(photocath_mt);
-
-   //**Al surface properties
-   G4double refl_Al[] = {1.,1.};
-   assert(sizeof(refl_Al) == sizeof(ephoton));
-   G4double effi_Al[] = {0, 0};
-   assert(sizeof(effi_Al) == sizeof(ephoton));
-   G4MaterialPropertiesTable* AlSurfaceProperty = new G4MaterialPropertiesTable();
-   AlSurfaceProperty->AddProperty("REFLECTIVITY",ephoton,refl_Al,num);
-   AlSurfaceProperty->AddProperty("EFFICIENCY",ephoton,effi_Al,num);
-   G4OpticalSurface* AlSurface = 
-    // new G4OpticalSurface("AlSurface",glisur,ground,dielectric_metal,1.);
-    new G4OpticalSurface("AlSurface",glisur,polished,dielectric_metal);
-   AlSurface -> SetMaterialPropertiesTable(AlSurfaceProperty);
+  photocath_opsurf->SetMaterialPropertiesTable(photocath_mt);*/
 
   //**Create logical skin surfaces
+  //new G4LogicalSkinSurface("Al_Surface", alShieldingLV, AlSurface);
+ 
 
-  new G4LogicalSkinSurface("photocath_surf",fPhotocath_log,photocath_opsurf);
-  new G4LogicalSkinSurface("Al_Surface", fLogicAl, AlSurface);
+  new G4LogicalSkinSurface("Al_Surface", fLogicCal, AlSurface);
+  //new G4LogicalSkinSurface("Taped_Surface", alPlateLV, TapedSurface); //(This is good)
+  //new G4LogicalSkinSurface("photocath_surf",lgTubeLV,TapedSurface);
 }
 
 
@@ -490,7 +622,20 @@ void DetectorConstruction::SetAbsorMaterial(const G4String& material)
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void DetectorConstruction::ConstructSDandField()
-{}
+{ 
+   /* SensitiveDetector* pmtSD = new SensitiveDetector*("PMTSD");
+    G4SDManager::GetSDMpointer()->AddNewDetector(pmtSD);
+    SetSensitiveDetector("LGTubeLV", pmtSD);*/
+
+
+
+ SensitiveDetector* aSD = new SensitiveDetector("SimSD");
+ // cathodeLV->SetSensitiveDetector(aSD);
+  lgTubeLV->SetSensitiveDetector(aSD);
+  G4SDManager* sdManager = G4SDManager::GetSDMpointer();
+  sdManager->AddNewDetector(aSD);
+
+}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
