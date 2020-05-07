1f8e9cd091275f32bdf24e676785aafec578237a
diegoih98 <53347276+diegoih98@users.noreply.github.com>
2020-05-07 18:07:16 -0500

    Add files via upload

---
+++ include/SensitiveDetector.hh
@@ -0,0 +1,28 @@
+
+#ifndef SensitiveDetector_HH
+#define SensitiveDetector_HH 1
+
+#include "G4VSensitiveDetector.hh"
+
+//class EventAction;
+
+class SensitiveDetector : public G4VSensitiveDetector
+{
+    public:
+        SensitiveDetector(G4String name);
+        ~SensitiveDetector();
+        
+        G4double GetQE(G4double energy);    
+    
+        void Initialize(G4HCofThisEvent*);
+        G4bool ProcessHits(G4Step* aStep, G4TouchableHistory*);
+        void EndOfEvent(G4HCofThisEvent*);
+    
+    private:
+        G4double qe_nhits;
+        G4double p2c;
+        G4double qe; 
+        //G4double E;
+};
+
+#endif
