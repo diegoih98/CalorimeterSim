223434be92c6fb40881bff4e219838ef429e9a7a
diegoih98 <53347276+diegoih98@users.noreply.github.com>
2020-05-07 23:29:53 -0500

    Update RunAction.cc

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -1,37 +1,5 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-/// \file RunAction.cc
-/// \brief Implementation of the RunAction class
-//
-// $Id: RunAction.cc 93562 2015-10-26 14:44:24Z gcosmo $
-// 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #include "RunAction.hh"
+
 #include "PrimaryGeneratorAction.hh"
 #include "HistoManager.hh"
 #include "Run.hh"
@@ -68,10 +36,10 @@ G4Run* RunAction::GenerateRun()
 
 void RunAction::BeginOfRunAction(const G4Run*)
 {  
-  // show Rndm status
+  // Show Rndm status
    if (isMaster)  G4Random::showEngineStatus();
    
-  // keep run condition
+  // Keep run condition
   if ( fPrimary ) { 
     G4ParticleDefinition* particle 
       = fPrimary->GetParticleGun()->GetParticleDefinition();
@@ -79,7 +47,7 @@ void RunAction::BeginOfRunAction(const G4Run*)
     fRun->SetPrimary(particle, energy);
   }
         
-  //histograms
+  //Histograms
   //
   G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
   if ( analysisManager->IsActive() ) {
@@ -93,14 +61,14 @@ void RunAction::EndOfRunAction(const G4Run*)
 { 
   if (isMaster) fRun->EndOfRun();  
   
-  // save histograms
+  // Save histograms
   G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
   if ( analysisManager->IsActive() ) {  
     analysisManager->Write();
     analysisManager->CloseFile();
   }    
  
-  // show Rndm status
+  // Show Rndm status
     if (isMaster) G4Random::showEngineStatus();
 }
 
